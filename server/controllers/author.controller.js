8bfbe4c083899589d12a21386a3830b16408dd09
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-19 23:09:52 -0700

    add author routes

---
+++ server/controllers/author.controller.js
@@ -0,0 +1,50 @@
+const { Author } = require('../models/author.model');
+
+module.exports.createauthor = (request, response) => {
+	const { name } = request.body;
+	Author.create({
+		name,
+	})
+		.then((author) => response.json(author))
+		.catch((err) => response.status(400).json(err)); // Will return error to user
+};
+
+// module.exports.findAllUsers = (req, res) => {
+// 	User.find()
+// 		.then((allDaUsers) => res.json({ users: allDaUsers }))
+// 		.catch((err) =>
+// 			res.json({ message: 'Something went wrong', error: err })
+// 		);
+// };
+
+// module.exports.findOneSingleUser = (req, res) => {
+// 	User.findOne({ _id: req.params.id })
+// 		.then((oneSingleUser) => res.json({ user: oneSingleUser }))
+// 		.catch((err) =>
+// 			res.json({ message: 'Something went wrong', error: err })
+// 		);
+// };
+
+// module.exports.createNewUser = (req, res) => {
+// 	User.create(req.body)
+// 		.then((newlyCreatedUser) => res.json({ user: newlyCreatedUser }))
+// 		.catch((err) =>
+// 			res.json({ message: 'Something went wrong', error: err })
+// 		);
+// };
+
+// module.exports.updateExistingUser = (req, res) => {
+// 	User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+// 		.then((updatedUser) => res.json({ user: updatedUser }))
+// 		.catch((err) =>
+// 			res.json({ message: 'Something went wrong', error: err })
+// 		);
+// };
+
+// module.exports.deleteAnExistingUser = (req, res) => {
+// 	User.deleteOne({ _id: req.params.id })
+// 		.then((result) => res.json({ result: result }))
+// 		.catch((err) =>
+// 			res.json({ message: 'Something went wrong', error: err })
+// 		);
+// };
---
+++ server/routes/author.routes.js
@@ -0,0 +1,13 @@
+const PersonController = require('../controllers/author.controller');
+module.exports = function (app) {
+	// ! READ ALL
+	app.get('/api/authors/', AuthorController.findAllAuthors);
+	// ! READ ONE
+	app.get('/api/authors/:id', AuthorController.findOneSingleAuthor);
+	// ! CREATE
+	app.post('/api/authors', PersonController.createPerson);
+	// ! PUT
+	app.put('/api/authors/:id', AuthorController.updateExistingAuthor);
+	// ! DELETE
+	app.delete('/api/authors/:id', AuthorController.deleteAnExistingAuthor);
+};
