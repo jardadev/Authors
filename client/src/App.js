09326ce1a9cd68e754c853ff22ea568292343f0a
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-21 13:59:13 -0700

    add Routes tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Main from './components/Main';
 function App() {
-	return <></>;
+	return (
+		<Routes>
+			<Route path='/product/:id' element={<Main />} />
+			{/* <Route path='/product/:id/edit' element={<Update />} />
+			
+			<Route path='*' element={<Main />} /> */}
+		</Routes>
+	);
 }
 
 export default App;
